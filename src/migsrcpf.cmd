5e10c79691836d6edde93dba4c8cc63d3cd43a6e
GajenderTyagi <gajender@programmers.io>
2020-10-13 12:10:53 +0530

    new cmd comment line

--- src/migsrcpf.cmd
+++ src/migsrcpf.cmd
@@ -8,3 +8,4 @@
                         PROMPT('Output Directory') ALWUNPRT(*NO) 
              PARM       KWD(CCSID) TYPE(*CHAR) LEN(10)  +
                         PROMPT('CCSID') ALWUNPRT(*NO) DFT(1252) 
+ /* comment line  */
